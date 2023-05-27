bd62b902738877e13e12998192189f1e53943863
Thuan0907 <jinkin0019@gmail.com>
2023-05-27 15:35:26 +0700

    update-react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React, { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login/LoginPage";
 
 const Layout = () => {
-  return <> Layout</>;
+  return <> Main page </>;
 };
 
 export default function App() {
-  return <Layout />;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <div>404 Not found</div>,
+    },
+    {
+      path: "/login",
+      element: (
+        <>
+          <LoginPage />
+        </>
+      ),
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,29 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginPage from "./pages/login/LoginPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <div>404 Not found</div>,
-  },
-  {
-    path: "/login",
-    element: (
-      <>
-        <LoginPage />
-      </>
-    ),
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
